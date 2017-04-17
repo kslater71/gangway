9b9b993d6a5834ace65b1bb5a6cb640404ab3497
kslater71 <kslater71@gmail.com>
2017-04-17 09:34:59 -0700

    Create page1.php

---
+++ page1.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+  <title>Gangway Page1</title>
+  <script src="//assets.adobedtm.com/e80b4f015d743e30fe9312f22bce1a7d162d6597/satelliteLib-eb8e1d8e6ea569e385e3ff826bd8a41215fb7cbb.js"></script>
+  <script>
+    // data layer object
+    var DLO={
+      pageName:'page1'
+    }
+  </script>
+
+</head>
+<body>
+  <b>Project Gangway</b>
+  
+<p>
+Nothing to see here, yet<br>
+  
+
+<script type="text/javascript">_satellite.pageBottom();</script>
+</body>
+</html>
