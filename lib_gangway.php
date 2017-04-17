3a371eda7b1baae9223528f755a5cce4f6c0085e
kslater71 <kslater71@gmail.com>
2017-04-17 14:36:44 -0700

    na

--- lib_gangway.php
+++ lib_gangway.php
@@ -10,8 +10,8 @@ function pageHeader() {
 
 
 function mainNav() {
- 	$html="<a href='default.php'>Home</a>@nbsp;@nbsp;";
- 	$html.="<a href='page1.php'>Page 1</a>@nbsp;@nbsp;";
+ 	$html="<a href='default.php'>Home</a>&nbsp;&nbsp;";
+ 	$html.="<a href='page1.php'>Page 1</a>&nbsp;&nbsp;";
  	$html.="<a href='page2.php'>Page 2</a>";
  	$html.="<p>";
  	echo($html);
